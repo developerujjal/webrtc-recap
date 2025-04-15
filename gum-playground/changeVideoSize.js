50ea32befa905f4a0b6ffb77a15d24b174c403f4
developerujjal <ujjal.vision02@gmail.com>
2025-04-16 01:44:12 +0600

    change video size dynamically using applyConstraints

--- gum-playground/changeVideoSize.js
+++ gum-playground/changeVideoSize.js
@@ -2,10 +2,26 @@ const getSupportedConstraints = navigator.mediaDevices.getSupportedConstraints()
 console.log(getSupportedConstraints)
 
 const changeVideoSize = (e) => {
-    console.log('changeVideoSize')
-    stream.getTracks().forEach((track) => {
-       const capabilities = track.getCapabilities();
-       console.log(capabilities);
+    // console.log('changeVideoSize')
+    // stream.getTracks().forEach((track) => {
+    //    const capabilities = track.getCapabilities();
+    //    console.log(capabilities);
+
+    // })
+
+    stream.getVideoTracks().forEach((track) => {
+        const capabilities = track.getCapabilities();
+        // console.log(capabilities);
+        const videoWidth = document.querySelector('#vid-width').value;
+        const videoHeight = document.querySelector('#vid-height').value;
+        const constraints = {
+            width: {exact: videoWidth < capabilities.width.max ? videoWidth : capabilities.width.max },
+            height: {exact: videoHeight < capabilities.height.max ? videoHeight : capabilities.height.max},
+        };
+
+        track.applyConstraints(constraints);
+
+        console.log(capabilities)
     })
 }
 
