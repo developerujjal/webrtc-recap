092609c3e878abf703f84c7781f84937c065e2df
developerujjal <ujjal.vision02@gmail.com>
2025-04-23 22:45:58 +0600

    add jwt for get link

--- teleLegalSite/teleLegal-backend/routes/routes.js
+++ teleLegalSite/teleLegal-backend/routes/routes.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const secret = 'aldkfsdlfksdlfkdsdklfasakdfj';
+
 
 router.get('/hello', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
+});
+
+router.get('/user-link', (req, res) => {
+    const data = {
+        name: 'barry',
+        appDate: Date.now()
+    };
+
+    const token = jwt.sign(data, secret);
+    res.send('http://localhost:3000/user-link?token=' + token);
+
+});
+
+router.get('/validate-link', (req, res) => {
+    const token =req.query.token;
+    if(!token){
+        return res.status(400).send('Link is not valid');
+    };
+   const decodedData = jwt.verify(token, secret,);
+
+   res.send(decodedData)
 });
 
 
